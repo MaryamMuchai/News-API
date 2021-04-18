9eaafd278fadf462d39351dfa66dedfbfe29a8b6
Marya <maryammuchai@gmail.com>
2021-04-18 14:20:13 +0300

    adding the movie details and its data in the views.py

--- app/main/views.py
+++ app/main/views.py
@@ -3,4 +3,20 @@ from . import main
 from ..request import get_news, process_results
 from ..models import Source, Article
 
-@
+@main.route('/')
+def index():
+    '''
+    root page function that returns the index page and its data
+    '''
+
+    science = get_news('science')
+    tech = get_news('tech')
+    health = get_news('health')
+    business = get_news('business')
+    sport = get_news('sport')
+    entertainment = get_entertainment('entertainment')
+
+    title = 'DailyBoomerang'
+    return render_template('index.html',title = title, business = business,  sport = sport, entertainment = entertainment, science = science
+    health = health, tech = tech)
+
