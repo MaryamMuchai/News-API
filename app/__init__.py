51e8f8155399ffe4b8f98c9a1074eec0710152bf
Marya <maryammuchai@gmail.com>
2021-04-18 11:57:10 +0300

    adding the config app

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,17 @@ from flask_bootstrap import Bootstrap
 from  config import config_options
 
 bootstrap = Bootstrap()
+
+def create_app(config_term):
+    app = Flask(__name__,instance_relative_config= True)
+
+    app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
