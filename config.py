0e69879cc00093dde0762b637d36416d79826249
Marya <maryammuchai@gmail.com>
2021-04-18 12:04:59 +0300

    adding the congif options

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+
+    NEWS_API_KEY =os.environ.get('NEWS_API_KEY)'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class Prodconfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':Prodconfig
+}
