5c209da9a9ffdf5e0dde360d803b8294a536b7f9
Marya <maryammuchai@gmail.com>
2021-04-18 15:39:37 +0300

    adding the base navbar block and editing macros file

--- app/main/views.py
+++ app/main/views.py
@@ -8,15 +8,13 @@ def index():
     '''
     root page function that returns the index page and its data
     '''
-
+    #Getting the popular news list
     science = get_news('science')
     tech = get_news('tech')
     health = get_news('health')
     business = get_news('business')
     sport = get_news('sport')
-    entertainment = get_entertainment('entertainment')
-
+    entertainment = get_news('entertainment')
     title = 'DailyBoomerang'
-    return render_template('index.html',title = title, business = business,  sport = sport, entertainment = entertainment, science = science
-    health = health, tech = tech)
+    return render_template('index.html',title = title, business = business,  sport = sport, entertainment = entertainment, science = science ,health = health, tech = tech)
 
